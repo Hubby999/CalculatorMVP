b4284b1bf619f49cfc8c32929f77dd04858a5c2b
unknown <jakhumam@gmail.com>
2019-12-17 10:29:08 +0700

    Initial commit

---
+++ app/src/main/java/com/blackswan/simplemvp/unittestMVP/presenter/MainPresenter.java
@@ -0,0 +1,24 @@
+package com.blackswan.simplemvp.unittestMVP.presenter;
+
+
+import com.blackswan.simplemvp.unittestMVP.model.MainModel;
+import com.blackswan.simplemvp.unittestMVP.view.MainView;
+
+
+public class MainPresenter {
+    private MainView view;
+
+    public MainPresenter(MainView view) {
+        this.view = view;
+    }
+
+    public double volume(double panjang, double lebar, double tinggi) {
+        return panjang * lebar * tinggi;
+    }
+
+    public void hitungVolume(double panjang, double lebar, double tinggi) {
+        double volume = volume(panjang, lebar, tinggi);
+        MainModel model = new MainModel(volume);
+        view.tampilVolume(model);
+    }
+}
---
+++ app/src/main/java/com/blackswan/simplemvp/unittestMVP/view/MainView.java
@@ -0,0 +1,8 @@
+package com.blackswan.simplemvp.unittestMVP.view;
+
+
+import com.blackswan.simplemvp.unittestMVP.model.MainModel;
+
+public interface MainView {
+    void tampilVolume(MainModel model);
+}
